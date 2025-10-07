885e96aa372fe86a4ce8e0c2cdf0f9582254827f
Brison Mondry <mondrybr@gmail.com>
2025-10-06 21:38:20 -0400

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+// desired functionality:
+// Ai Summarize using local model
+// Rename dated files to different date format
+// Create new nexus files that contain links to files that share a common theme
